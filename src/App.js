bb7f564340d94d2874797964fd7b65bd236ba0b8
LIU KEYI <1531194240@qq.com>
2024-05-23 00:06:48 +0800

    init commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import LoginPage from './JS/LoginPage'; // 确保这里的路径根据你的文件结构调整
+
+function App() {
+  return (
+    <div className="App">
+      <LoginPage />
+    </div>
+  );
+}
+
+export default App;
